26f9310173f818be8cd21f709ab95d72f7c68405
Paul Mietz Egli <paul@obscure.com>
2012-12-26 09:43:37 -0800

    CARBON-5: Android implementation of UI templates

--- android/example/app.js
+++ android/example/app.js
@@ -3,7 +3,11 @@ var C = Carbon = require('appersonlabs.carbon');
 
 C.UI.addTSS('ui/test1.tss');
 
-var result = Carbon.UI.load('ui/test_android.json');
+var result = Carbon.UI.load('ui/test_android.json', {
+  button_title: 'this button should work in a moment...',
+  label_text: 'I am a label, hurrah!',
+  alabel_text: 'this is a label on tab 1'
+});
 result.open();
 
 var button = Carbon.UI.create({"Button": {
@@ -19,11 +23,6 @@ result.add(button);
 
 // basic test
 var testButton = C.UI.find('#testButton');
-
-// var testButton = C.UI.find('#testButton');
-// testButton.addEventListener('click', function(e) {
-//     alert('hello!');
-// });
 setTimeout(function() {
     testButton.title = 'Click me now!';
     
